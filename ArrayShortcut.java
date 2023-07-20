f399bd8d785812e06d98b9890da04f7441d65429
Kirthiv2018 <42927009+Kirthiv2018@users.noreply.github.com>
2023-07-20 08:26:35 +0530

    Add files via upload

---
+++ ArrayShortcut.java
@@ -0,0 +1,24 @@
+package workoutprograms;
+
+public class ArrayShortcut {
+
+	public static void main(String[] args) {
+
+		
+		int[] anArray = { 
+			    100, 200, 300,
+			    400, 500, 600, 
+			    700, 800, 900, 1000
+			};
+		
+		for (int i=0; i<anArray.length; i++) {
+			
+			System.out.println(anArray[i]);
+			
+		}
+		
+		
+		
+	}
+
+}
